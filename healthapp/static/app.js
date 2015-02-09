c050142e0e52dd112b5ac9983b0b0ab3a5a99809
r@imambaks.nl <dangerzz>
2015-02-09 19:01:42 +0100

    Going DJANGO!!

---
+++ healthapp/static/app.js
@@ -0,0 +1,28 @@
+/**
+ * Created by imambaksr on 1/6/2015.
+ */
+
+'use strict';
+angular.module('myApp', ['ui.bootstrap', 'ui.grid', 'ngRoute'])
+/*		.config(['$routeProvider', function ($routeProvider) {
+
+				$routeProvider
+						.when('/food-data', {
+								templateUrl: 'templates/fooddata.html'
+						})
+						.when('/foodlog', {
+								templateUrl: 'templates/foodlog.html'
+						})
+						.when('/recipes', {
+								templateUrl: 'templates/recipes.html'
+						})
+						.otherwise({
+								redirectTo: '/'
+						})
+
+
+		}])*/
+
+		.config(function ($httpProvider) {
+				delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		});
