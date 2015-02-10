5a4a21600efe9177c32bac49a0b2d38239f99248
Rashied Imambaks <r@imambaks.nl>
2015-02-10 19:45:55 +0100

    rest-framework included

--- healthapp/urls.py
+++ healthapp/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'healthapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'healthapp.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'healthapp.views.home_page', name='home_page')
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', 'healthapp.views.home_page', name='home_page'),
+                       url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
