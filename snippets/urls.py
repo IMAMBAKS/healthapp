bd48b6064e2a2ac3751bbc4a8b58fd8b8eced2e3
Rashied Imambaks <r@imambaks.nl>
2015-02-13 21:25:19 +0100

    rest-framework included

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,1 +1,11 @@
 __author__ = 'Administrator'
+from django.conf.urls import url
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
