2a51391abb47cdb6447a90309478ef816ce9af19
Rashied Imambaks <r@imambaks.nl>
2015-02-13 14:14:57 +0100

    rest-framework included

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
 # Create your views here.
