d85138629c8fea89c0b951d79137708b86ff6a78
Rashied Imambaks <r@imambaks.nl>
2015-02-22 19:44:31 +0100

    rest-framework included

--- snippets/views.py
+++ snippets/views.py
@@ -15,7 +15,7 @@ class SnippetList(APIView):
     def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer)
+        return Response(serializer.data)
 
     def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
